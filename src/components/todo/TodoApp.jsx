ccfb8dc06dcb80efe19fba102cec5eef885e2b72
BoYoung00 <qhdud20@naver.com>
2024-01-04 15:29:13 +0900

    24/01/04 리액트 공부, 로그인 기능, 라우팅

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,30 +1,64 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import './TodoApp.css'
+
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            //Todo Management Application
-            <LoginComponent />
-            {/* <WelcomeComponent /> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LoginComponent />}></Route>
+                    <Route path='/login' element={<LoginComponent />}></Route>
+                    <Route path='/welcome' element={<WelcomeComponent />}></Route>
+                </Routes>
+            </BrowserRouter> 
         </div>
     )
 }
 
 function LoginComponent() {
+    const [username, setUsername] = useState('kim')
+    const [password, setPassword] = useState('')
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+    const navigate = useNavigate();
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+    function handleSubmit() {
+        if(username === 'kim' && password === 'pw123') {
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+            navigate('/welcome')
+        } else {
+            setShowErrorMessage(true)
+            setShowSuccessMessage(false)
+        }
+    }
+
     return (
         <div className="Login">
+            {showSuccessMessage && <div className="successMessage">로그인 성공</div>}
+            {showErrorMessage && <div className="errorMessage">로그인 실패</div>}
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username" />
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="text" name="password" />
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                <button type="button" name="login">로그인</button>
+                <button type="button" name="login" onClick={handleSubmit}>로그인</button>
                 </div>
             </div>
-            button
         </div>
     )
 }
